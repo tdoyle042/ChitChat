98413dae3d59c496e2957192470294c77bc78856
Chanamon <cratanal@andrew.cmu.edu>
2014-11-09 17:59:42 -0500

    Basic Front-end implementation
    Not yet linked to server for calling pages
    simple styling and interfaces

---
+++ public/js/script.js
@@ -0,0 +1,32 @@
+$(function() {   // when document is ready
+
+	/* create_chat screen */
+	$('#create_chat').click(function(){
+		$.ajax({
+			url: "create_chat",
+			type: "get",
+			data: {
+				
+			},
+			success: function(data) {
+				
+			}
+		});
+	});
+
+	/* join_chat screen */
+	$('#join_chat').click(function(){
+
+	});
+
+});
+
+/* create_chat screen */
+function updateTime(time) {
+	console.log("poo");
+	document.querySelector('#volume').value = time;
+}
+
+function updateRange(range){
+
+}
